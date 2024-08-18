2cc776c91e131d1ba88912459df33880a31a1ff6
Dhruv Burada <buradaddhruv35@gmail.com>
2024-08-18 08:36:10 +0530

    feat(ui): update login screen design and disabled best practice suggestions

--- lib/screens/student_login_screen.dart
+++ lib/screens/student_login_screen.dart
@@ -1,111 +1,177 @@
 import 'package:flutter/material.dart';
-import 'package:quick_exit/screens/admin_login_screen.dart';
-import 'package:quick_exit/screens/gate_pass_request.dart';
 
 class StudentLogin extends StatefulWidget {
-  const StudentLogin({super.key});
-
   @override
   State<StudentLogin> createState() => _StudentLoginState();
 }
 
 class _StudentLoginState extends State<StudentLogin> {
-  TextEditingController enrollmentController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
+  final TextEditingController _enrollmentController = TextEditingController();
+
+  final TextEditingController _passwordController = TextEditingController();
+
+  bool isPasswordVisible = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: enrollmentController,
-              decoration: InputDecoration(
-                hintText: "Enrollment Number",
-                labelText: "Enrollment Number",
-                prefixIcon: Icon(
-                  Icons.person,
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(2),
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 25,
-            ),
-            TextField(
-              controller: passwordController,
-              decoration: InputDecoration(
-                hintText: "Password",
-                labelText: "Password",
-                prefixIcon: Icon(
-                  Icons.lock,
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(2),
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 45,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => GatePassRequest(),
+      body: Column(
+        children: [
+          // Red container with wavy bottom edge and Welcome Text
+          ClipPath(
+            clipper: WaveClipper(),
+            child: Container(
+              width: double
+                  .infinity, // width of red container adjusted with this expanded dint worked
+              height: 400,
+              color: Color(0xFFFF3B30),
+              child: Padding(
+                padding:
+                    const EdgeInsets.only(top: 75, left: 24.0, right: 24.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Welcome to QuickExit',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  );
-                });
-              },
-              child: Text("Login"),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.purple.shade700,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                padding: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
-              ),
-            ),
-            SizedBox(
-              height: 25,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => AdminLogin(),
+                    SizedBox(height: 21),
+                    Text(
+                      'Seamless and secure gate passes at your fingertips.',
+                      style: TextStyle(
+                        color: Colors.white70,
+                        fontSize: 16,
+                        fontStyle: FontStyle.italic,
+                      ),
                     ),
-                  );
-                });
-              },
-              child: Text(
-                "Employee Login",
-                style: TextStyle(color: Colors.black),
-              ),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.purple.shade100,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
+                    SizedBox(height: 13),
+                    Text(
+                      'Login to Continue',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontStyle: FontStyle.italic,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
                 ),
-                padding: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
               ),
             ),
-          ],
-        ),
+          ),
+
+          // Form Field Container with Form Fields
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 50),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  //Enrollment number input field
+                  TextField(
+                    controller: _enrollmentController,
+                    decoration: InputDecoration(
+                      labelText: 'Enrollment Number',
+                      labelStyle: TextStyle(color: Colors.grey),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      prefixIcon: Icon(Icons.person_outline),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+
+                  //Password input field
+                  TextField(
+                    obscureText: isPasswordVisible,
+                    controller:
+                        _passwordController, //made password visiblity dynamic
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      labelStyle: TextStyle(color: Colors.grey),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      prefixIcon: Icon(Icons.lock_outline),
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          isPasswordVisible
+                              ? Icons.visibility_off
+                              : Icons.visibility,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            isPasswordVisible = !isPasswordVisible;
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 30),
+
+                  //Login Button
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xFFFF3B30),
+                      foregroundColor: Colors.white,
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 100, vertical: 15),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                    ),
+                    child: Text('Login'),
+                  ),
+                  SizedBox(height: 20),
+
+                  //Employee Login Button
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color.fromARGB(255, 237, 242, 244),
+                      foregroundColor: Color.fromARGB(255, 43, 44, 66),
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 65, vertical: 15),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                    ),
+                    child: Text('Employee Login'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
+}
+
+class WaveClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.lineTo(0.0, size.height - 60);
+
+    // First curve (downward)
+    path.quadraticBezierTo(
+        size.width / 4, size.height - 100, size.width / 2, size.height - 60);
+
+    // Second curve (upward)
+    path.quadraticBezierTo(
+        3 * size.width / 4, size.height - 20, size.width, size.height - 60);
+
+    path.lineTo(size.width, 0.0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
 }
