a885a19c14addecb341136f60473f391790552e1
Dhruv Burada <buradaddhruv35@gmail.com>
2024-08-31 10:29:40 +0530

    feat(guard-dashboard): add functional navigation bar

---
+++ lib/screens/Guard_Screens/IssueGatepass.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Issuegatepass extends StatefulWidget {
+  const Issuegatepass({super.key});
+
+  @override
+  State<Issuegatepass> createState() => _IssuegatepassState();
+}
+
+class _IssuegatepassState extends State<Issuegatepass> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/screens/Guard_Screens/Requests.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Requests extends StatefulWidget {
+  const Requests({super.key});
+
+  @override
+  State<Requests> createState() => _RequestsState();
+}
+
+class _RequestsState extends State<Requests> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
