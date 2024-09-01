30b0797fa93f2bc64e853f86699b82ae11c1e5b6
Dhruv Burada <buradadhruv35@gmail.com>
2024-09-01 10:15:10 +0530

    feat(ui) : implement basic design of Guard's issue Gatepass screen

--- lib/screens/Guard_Screens/IssueGatepass.dart
+++ lib/screens/Guard_Screens/IssueGatepass.dart
@@ -8,8 +8,54 @@ class Issuegatepass extends StatefulWidget {
 }
 
 class _IssuegatepassState extends State<Issuegatepass> {
-  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    double height = MediaQuery.of(context).size.height;
+
+    return SafeArea(
+        child: Container(
+      decoration: BoxDecoration(color: Colors.red),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 30, left: 24, right: 24),
+            child: Container(
+              height: height * 0.18,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Issue Gate Pass',
+                    style: TextStyle(
+                        fontSize: 26,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                      "Quickly authorize Entry or Exit for\nstudents and visitors",
+                      style: TextStyle(
+                        color: Colors.white70,
+                        fontSize: 15,
+                        fontStyle: FontStyle.italic,
+                        letterSpacing: 1,
+                      ))
+                ],
+              ),
+            ),
+          ),
+          Container(
+            height: height - (height * 0.18) - 100,
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(50),
+                    topRight: Radius.circular(50))),
+          )
+        ],
+      ),
+    ));
   }
 }
