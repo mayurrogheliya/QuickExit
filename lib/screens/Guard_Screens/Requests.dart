1ba549a95a34ed5f5bd8516cc3c28edfe8f289b3
Mayur Rogheliya <mayurrogheliya@gmail.com>
2024-09-15 23:32:41 +0530

    Revert "feat: add reusable custom card component with action buttons and styling"
    This reverts commit a986831ac639ebe1667d9a4c35cb72fc6315a53f.

--- lib/screens/Guard_Screens/Requests.dart
+++ lib/screens/Guard_Screens/Requests.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:quick_exit/screens/Guard_Screens/CustomCard.dart';
-import 'package:quick_exit/screens/Guard_Screens/Header.dart';
 
 class Requests extends StatefulWidget {
   const Requests({super.key});
@@ -12,30 +10,6 @@ class Requests extends StatefulWidget {
 class _RequestsState extends State<Requests> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView(
-        children: [
-          Header(title: "Gate Pass Requests"),
-          CustomCard(
-            name: "Mayur Rogheliya",
-            destination: "Rajkot",
-            exitDate: "15-09-2024",
-            reason: "Medical",
-          ),
-          CustomCard(
-            name: "Paul Stephen",
-            destination: "Ahmedabad",
-            exitDate: "30-09-2024",
-            reason: "Shopping",
-          ),
-          CustomCard(
-            name: "Dhruv Burada",
-            destination: "Tramba",
-            exitDate: "17-09-2024",
-            reason: "Marriage",
-          ),
-        ],
-      ),
-    );
+    return const Placeholder();
   }
 }
