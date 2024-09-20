eafe139321a7cc3bf3bd514f19fecfd7b4508558
Mayur Rogheliya <mayurrogheliya@gmail.com>
2024-09-20 11:07:00 +0530

    feat(history-gatepass): Implement history gatepass card component with conditional button rendering

--- lib/screens/Guard_Screens/History.dart
+++ lib/screens/Guard_Screens/History.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:quick_exit/screens/Guard_Screens/Header.dart';
+import 'package:quick_exit/screens/Guard_Screens/CustomCard.dart';
 
 class History extends StatefulWidget {
   const History({super.key});
@@ -10,6 +12,33 @@ class History extends StatefulWidget {
 class _HistoryState extends State<History> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: ListView(
+        children: [
+          Header(title: "Gate Pass History"),
+          CustomCard(
+            leave: "Extended Leave",
+            name: "Mayur Rogheliya",
+            destination: "Rajkot",
+            exitDate: "15-09-2024",
+            buttonType: ButtonType.rightAndCall,
+          ),
+          CustomCard(
+            leave: "Day Leave",
+            name: "Paul Stephen",
+            destination: "Ahmedabad",
+            exitDate: "30-09-2024",
+            buttonType: ButtonType.rightAndCall,
+          ),
+          CustomCard(
+            leave: "Extended Leave",
+            name: "Dhruv Burada",
+            destination: "Tramba",
+            exitDate: "17-09-2024",
+            buttonType: ButtonType.rightAndCall,
+          ),
+        ],
+      ),
+    );
   }
 }
