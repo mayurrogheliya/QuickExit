e52cc8a323d279e77e66278a53bba0abe22f5407
Mayur Rogheliya <mayurrogheliya@gmail.com>
2024-09-20 17:35:12 +0530

    refactor: Integrate VisitorCard and NavBox components into the current screen

--- lib/screens/Guard_Screens/History.dart
+++ lib/screens/Guard_Screens/History.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:quick_exit/screens/Guard_Screens/Header.dart';
 import 'package:quick_exit/screens/Guard_Screens/CustomCard.dart';
+import 'package:quick_exit/screens/Guard_Screens/NavBox.dart';
+import 'package:quick_exit/screens/Guard_Screens/VisitorCard.dart';
 
 class History extends StatefulWidget {
   const History({super.key});
@@ -16,26 +18,53 @@ class _HistoryState extends State<History> {
       body: ListView(
         children: [
           Header(title: "Gate Pass History"),
-          CustomCard(
-            leave: "Extended Leave",
-            name: "Mayur Rogheliya",
-            destination: "Rajkot",
-            exitDate: "15-09-2024",
-            buttonType: ButtonType.rightAndCall,
-          ),
-          CustomCard(
-            leave: "Day Leave",
-            name: "Paul Stephen",
-            destination: "Ahmedabad",
-            exitDate: "30-09-2024",
-            buttonType: ButtonType.rightAndCall,
-          ),
-          CustomCard(
-            leave: "Extended Leave",
-            name: "Dhruv Burada",
-            destination: "Tramba",
-            exitDate: "17-09-2024",
-            buttonType: ButtonType.rightAndCall,
+          NavBox(
+            title1: "Students",
+            title2: "Visitors",
+            screen1: Column(
+              children: [
+                CustomCard(
+                  leave: "Extended Leave",
+                  name: "Mayur Rogheliya",
+                  destination: "Rajkot",
+                  exitDate: "15-09-2024",
+                  buttonType: ButtonType.rightAndCall,
+                ),
+                CustomCard(
+                  leave: "Day Leave",
+                  name: "Paul Stephen",
+                  destination: "Ahmedabad",
+                  exitDate: "30-09-2024",
+                  buttonType: ButtonType.rightAndCall,
+                ),
+                CustomCard(
+                  leave: "Extended Leave",
+                  name: "Dhruv Burada",
+                  destination: "Tramba",
+                  exitDate: "17-09-2024",
+                  buttonType: ButtonType.rightAndCall,
+                ),
+              ],
+            ),
+            screen2: Column(
+              children: [
+                VisitorCard(
+                  reason: "Appointment",
+                  name: "Meet Pitroda",
+                  timestamp: "10:00 AM",
+                ),
+                VisitorCard(
+                  reason: "Chief Guest",
+                  name: "Brijesh Sakhiya",
+                  timestamp: "12:00 AM",
+                ),
+                VisitorCard(
+                  reason: "Admission",
+                  name: "Archil Gajera",
+                  timestamp: "10:15 AM",
+                ),
+              ],
+            ),
           ),
         ],
       ),
